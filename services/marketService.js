b7aedb8141ec256cc9ba49342b1f88cbfa03f587
Davidhuecker-bit <David.huecker@web.de>
2026-02-19 08:21:11 +0100

    Update marketService.js

--- services/marketService.js
+++ services/marketService.js
@@ -1,9 +1,8 @@
 const { fetchQuote } = require("./providerService");
-const { buildHQSResponse } = require("./hqsEngine");
+const { buildHQSResponse } = require("../hqsEngine");
 
 async function getMarketData(symbol) {
   const rawData = await fetchQuote(symbol);
-
   return rawData.map(item => buildHQSResponse(item));
 }
 
